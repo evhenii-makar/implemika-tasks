a33c8bfbba27c020701cb9d47af55fe28f2c718e
YevheniiMakar <maggogg88@gmail.com>
2020-08-10 21:56:00 +0300

    frame created

--- src/main/java/com/imaginarium/webstudio/Main.java
+++ src/main/java/com/imaginarium/webstudio/Main.java
@@ -1,4 +1,28 @@
 package com.imaginarium.webstudio;
 
+import com.imaginarium.webstudio.minimumСost.node.NodeInGraph;
+import com.imaginarium.webstudio.minimumСost.service.DijkstrasAlgorithm;
+import com.imaginarium.webstudio.minimumСost.service.DijkstrasAlgorithmImpl;
+import com.imaginarium.webstudio.minimumСost.service.NodeService;
+import com.imaginarium.webstudio.minimumСost.service.NodeServiceImpl;
+
+import java.util.HashMap;
+
 public class Main {
+    public static void main(String []args){
+        NodeService nodeService = new NodeServiceImpl();
+        DijkstrasAlgorithm dijkstrasAlgorithm = new DijkstrasAlgorithmImpl();
+        HashMap <Integer,NodeInGraph> nodeMap = nodeService.createNodeMap(4);
+        System.out.println(nodeMap);
+        for (int key: nodeMap.keySet()){
+            System.out.println(nodeMap.get(key).getIndexNodeAndCost());
+        }
+        dijkstrasAlgorithm.addGraphMap(nodeMap);
+
+        System.out.println(dijkstrasAlgorithm.getMinimumPathLength(nodeMap.get(1),nodeMap.get(4)));
+
+
+    }
+
+
 }
---
+++ src/test/java/com/imaginarium/webstudio/minimumСost/service/DijkstrasAlgorithmImplTest.java
@@ -0,0 +1,16 @@
+package com.imaginarium.webstudio.minimumСost.service;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DijkstrasAlgorithmImplTest {
+
+
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getMinimumPathLength() {
+    }
+}
